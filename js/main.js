6836ee505abe43433d7a03528a71d4c4030eced1
Murod1303 <murodshernazaroff@gmail.com>
2023-08-03 13:49:33 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,122 @@
+var elList = document.querySelector(".poke__list")
+
+
+
+pokemons.forEach(function(item, index, arr) {
+  var liElement = document.createElement("li");
+  var spanWeigthElement = document.createElement("span");
+  var spanHeigthElement = document.createElement("span");
+  var spanEggsElement = document.createElement("span");
+  var spanSparwElement = document.createElement("span");
+  var pokeFlexElement = document.createElement("div");
+  var pokeWHElement = document.createElement("div");
+  var pokeEggSparwElement = document.createElement("div");
+  var pokeImgElement = document.createElement("img");
+  var pokeNameElement = document.createElement("p")
+  
+  pokeImgElement.classList.add("poke__img")
+  pokeImgElement.setAttribute("src", `${item.img}`);
+  pokeImgElement.setAttribute("alt", `${item.name}`);
+
+  pokeImgElement.width = "150";
+  pokeImgElement.height = "100"
+  
+  spanWeigthElement.textContent ="weight" + ": " + `${item.weight}`;
+  
+  spanHeigthElement.textContent = "height" + ": " + `${item.height}`;
+  
+  spanEggsElement.textContent = "Egg" + ": " + `${item.egg}`;
+  
+  spanSparwElement.textContent = "Spawn time" + ": " + `${item.spawn_time}`;
+
+  pokeNameElement.textContent = item.name
+
+  pokeNameElement.classList.add("poke__name")
+  
+  pokeWHElement.classList.add("poke__wh")
+  pokeWHElement.appendChild(spanHeigthElement);
+  pokeWHElement.appendChild(spanWeigthElement);
+  
+  pokeEggSparwElement.classList.add("poke__egs")
+  pokeEggSparwElement.appendChild(spanSparwElement);
+  pokeEggSparwElement.appendChild(spanEggsElement);
+
+  pokeFlexElement.classList.add("poke__spans");
+  pokeFlexElement.appendChild(pokeEggSparwElement);
+  pokeFlexElement.appendChild(pokeWHElement);
+  
+  liElement.classList.add("poke__item");
+  liElement.appendChild(pokeImgElement);
+  liElement.appendChild(pokeFlexElement);
+  liElement.appendChild(pokeNameElement);
+
+  elList.append(liElement);
+  
+});
+
+
+
+
+
+
+
+var colors = new Array(
+  [62,35,255],
+  [60,255,60],
+  [255,35,98],
+  [45,175,230],
+  [255,0,255],
+  [255,128,0]);
+
+var step = 0;
+//color table indices for: 
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
+
+//transition speed
+var gradientSpeed = 0.002;
+
+function updateGradient()
+{
+  
+  if ( $===undefined ) return;
+  
+var c0_0 = colors[colorIndices[0]];
+var c0_1 = colors[colorIndices[1]];
+var c1_0 = colors[colorIndices[2]];
+var c1_1 = colors[colorIndices[3]];
+
+var istep = 1 - step;
+var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+ $('#gradient').css({
+   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+  
+  step += gradientSpeed;
+  if ( step >= 1 )
+  {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+    
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    
+  }
+}
+
+setInterval(updateGradient,10);
